f619d33e04111ccd94d2fdd8bd583624473cce97
Ernesto Vanegas <ernestovanegas9@gmail.com>
2024-04-04 19:23:35 -0600

    Entitdad Cita

---
+++ UMG-Clinica/Models/Cita.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace UMG_Clinica.Models {
+    [Table("Cita")]
+    public class Cita {
+        [Key]
+        [Column("ID_Cita")]
+        public int idCita { get; set; }
+        [Column("Fecha_Hora")]
+        public DateTime fechaHora { get; set; }
+        [Column("Estado")]
+        public string estado { get; set; }
+        [Column("ID_Clinica")]
+        public int idClinica { get; set; }
+        [Column("ID_Paciente")]
+        public int idPaciente { get; set; }
+        [Column("ID_Empleado")]
+        public int idEmpleado { get; set; }
+    }
+}
