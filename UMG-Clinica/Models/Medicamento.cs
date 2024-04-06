ef556aa82956be989c19c5a3caa4769870440b1e
ERICK HUMBERTO JOSE MORALES FLORES <emoralesf9@miumg.edu.gt>
2024-04-06 08:00:20 -0600

    Cambios Humberto

---
+++ UMG-Clinica/Models/Medicamento.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Analytics.Interfaces;
+using Microsoft.Analytics.Types.Sql;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace UMG_Clinica.Models
+{
+    [Table("Cita")]
+    public class Medicamento
+    {
+        [Key]
+        public int ID_Medicamento { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Nombre { get; set; }
+
+        [StringLength(255)]
+        public string Descripcion { get; set; }
+
+        public int Stock { get; set; }
+
+        [Column(TypeName = "decimal(10, 2)")]
+        public decimal Precio { get; set; }
+    }
+}
