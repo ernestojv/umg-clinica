e040f174169666c5d2131d310255174b82d2ac0b
ANDRES DONIS <angeldonis06@gmail.com>
2024-05-01 22:31:22 -0600

    para guardar proceso controlador de clinica

---
+++ UMG-Clinica/Controllers/clinica.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+using UMG_Clinica.Models;
+using HttpDeleteAttribute = System.Web.Http.HttpDeleteAttribute;
+using RouteAttribute = System.Web.Http.RouteAttribute;
+
+namespace UMG_Clinica.Controllers
+{
+    [System.Web.Http.RoutePrefix("api/usuario")]
+    public class clinicaController : ApiController
+    {
+        private readonly ClinicaDbContext _dbContext = new ClinicaDbContext();
+
+        
+        
+
