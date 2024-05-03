4040029feb832de0fe2c90e5168e6b62063335dd
LaloPolanco <eduardopolanco511@gmail.com>
2024-05-03 08:09:56 -0600

    Controllers Enfermedad y ConsultaE

---
+++ UMG-Clinica/Controllers/ConsultaEnfermedadController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+using UMG_Clinica.Models;
+using HttpDeleteAttribute = System.Web.Http.HttpDeleteAttribute;
+using RouteAttribute = System.Web.Http.RouteAttribute;
+
+namespace UMG_Clinica.Controllers
+{
+    [System.Web.Http.RoutePrefix("api/consultaenfermedad")]
+    public class ConsultaEnfermedadController : ApiController
+    {
+        private readonly ClinicaDbContext _dbContext = new ClinicaDbContext();
+
+        [Route("")]
+        public IHttpActionResult Get()
+        {
+            try
+            {
+                var consultaE = _dbContext.Consulta_Enfermedad.ToList();
+                return Ok(consultaE);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [Route("{ID}")]
+        public IHttpActionResult Get(int id)
+        {
+            try
+            {
+                var consultaE = _dbContext.Consulta_Enfermedad.Find(id);
+                if (consultaE == null)
+                {
+                    var responseMessage = new HttpResponseMessage(HttpStatusCode.NotFound)
+                    {
+                        Content = new StringContent("Consulta no encontrada")
+                    };
+                    return ResponseMessage(responseMessage);
+                }
+                return Ok(consultaE);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [Route("")]
+        public IHttpActionResult Post([FromBody] Consulta_Enfermedad consultaE)
+        {
+            try
+            {
+                _dbContext.Consulta_Medicina.Add(consultaE);
+                _dbContext.SaveChanges();
+                return Ok(consultaE);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [Route("")]
+        public IHttpActionResult Put([FromBody] Consulta_Enfermedad consultaE)
+        {
+            try
+            {
+                _dbContext.Entry(consultaE).State = System.Data.Entity.EntityState.Modified;
+                _dbContext.SaveChanges();
+                return Ok(consultaE);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpDelete]
+        [Route("{ID}")]
+        public IHttpActionResult Delete(int id)
+        {
+            try
+            {
+                var consultaE = _dbContext.Consulta_Enfermedad.Find(id);
+                if (consultaE == null)
+                {
+                    var responseMessage = new HttpResponseMessage(HttpStatusCode.NotFound)
+                    {
+                        Content = new StringContent("Consulta no encontrada")
+                    };
+                    return ResponseMessage(responseMessage);
+                }
+                _dbContext.Consulta_Enfermedad.Remove(consultaE);
+                _dbContext.SaveChanges();
+                return Ok(consultaE);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+    }
+}
---
+++ UMG-Clinica/Controllers/EnfermedadController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+using UMG_Clinica.Models;
+using HttpDeleteAttribute = System.Web.Http.HttpDeleteAttribute;
+using RouteAttribute = System.Web.Http.RouteAttribute;
+
+namespace UMG_Clinica.Controllers
+{
+    [System.Web.Http.RoutePrefix("api/enfermedad")]
+
+    public class EnfermedadController : ApiController
+    {
+        private readonly ClinicaDbContext _dbContext = new ClinicaDbContext();
+
+        [Route("")]
+        public IHttpActionResult Get()
+        {
+            try
+            {
+                var Enfermedades = _dbContext.Enfermedad.ToList();
+                return Ok(Enfermedades);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [Route("{ID_Enfermedad}")]
+        public IHttpActionResult Get(int id)
+        {
+            try
+            {
+                var enfermedad = _dbContext.Enfermedad.Find(id);
+                if (enfermedad == null)
+                {
+                    var responseMessage = new HttpResponseMessage(HttpStatusCode.NotFound)
+                    {
+                        Content = new StringContent("Enfermedad no encontrada")
+                    };
+                    return ResponseMessage(responseMessage);
+                }
+                return Ok(enfermedad);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [Route("")]
+        public IHttpActionResult Post([FromBody] Enfermedad enfermedad)
+        {
+            try
+            {
+                var medicamentoDb = _dbContext.Medicamento.Find(enfermedad.ID_Enfermedad);
+                if (medicamentoDb != null)
+                {
+                    var responseMessage = new HttpResponseMessage(HttpStatusCode.Conflict)
+                    {
+                        Content = new StringContent("Enfermedad ya registrada")
+                    };
+                    return ResponseMessage(responseMessage);
+                }
+                _dbContext.Enfermedad.Add(Enfermedad);
+                _dbContext.SaveChanges();
+                return Ok(enfermedad);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [Route("")]
+        public IHttpActionResult Put([FromBody] Enfermedad enfermedad)
+        {
+            try
+            {
+                _dbContext.Entry(enfermedad).State = System.Data.Entity.EntityState.Modified;
+                _dbContext.SaveChanges();
+                return Ok(enfermedad);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpDelete]
+        [Route("{ID_Enfermedad}")]
+        public IHttpActionResult Delete(int id)
+        {
+            try
+            {
+                var enfermedad = _dbContext.Enfermedad.Find(id);
+                if (enfermedad == null)
+                {
+                    var responseMessage = new HttpResponseMessage(HttpStatusCode.NotFound)
+                    {
+                        Content = new StringContent("Enfermedad no encontrada")
+                    };
+                    return ResponseMessage(responseMessage);
+                }
+                _dbContext.Enfermedad.Remove(enfermedad);
+                _dbContext.SaveChanges();
+                return Ok(enfermedad);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+    }
+}
